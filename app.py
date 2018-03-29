14ab8d62cd585cdd2813435f6a7b183bafc7a1c2
mingdachen21 <37481703+mingdachen21@users.noreply.github.com>
2018-03-29 14:01:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -87,22 +87,22 @@ def makeWebhookCollage(req):
     #if city is None:
         #return None
     data = pd.read_csv("data_test.csv",header = 0)
-    #data1 = data.groupby(['state']).get_group(state)
-    #data2 = data1.groupby(['degree']).get_group(degree)
-    #data3 = data2.sort_values([major])
+    data1 = data.groupby(['state']).get_group(state)
+    data2 = data1.groupby(['degree']).get_group(degree)
+    data3 = data2.sort_values([major])
     
-    #speech1 = "the top 5 university for you:" + '\n' +\
-    #     "1." + data3['University Name'].iloc[1] +'\n'+\
-    #     "2." + data3['University Name'].iloc[2] +'\n'+\
-    #     "3." + data3['University Name'].iloc[3] +'\n'+\
-    #     "4." + data3['University Name'].iloc[4] +'\n'+\
-    #     "5." + data3['University Name'].iloc[5]
+    speech1 = "the top 5 university for you:" + '\n' +\
+         "1." + data3['University Name'].iloc[1] +'\n'+\
+         "2." + data3['University Name'].iloc[2] +'\n'+\
+         "3." + data3['University Name'].iloc[3] +'\n'+\
+         "4." + data3['University Name'].iloc[4] +'\n'+\
+         "5." + data3['University Name'].iloc[5]
     
     speech2 = "1.george mason university" 
-    #print(speech1)
+    print(speech1)
     print(speech2)
     return {
-       "speech": speech2,
+       "speech": speech1 + speech2,
        "displayText": "you get this right",
        # "data": data,
        # "contextOut": [],
